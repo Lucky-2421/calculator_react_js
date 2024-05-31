cd09ffb50ccede634583a6648f8832bd27492583
Lucky-2421 <lokesh.2032.singh@gamil.com>
2024-05-31 15:17:50 +0530

    2 digit opertaion as well as decimal also work

--- App.js
+++ App.js
@@ -1,64 +1,92 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 import * as math from "mathjs";
 
-import "./App.css";
+
+import './App.css';
 import Button from "./components/Button";
 import Input from "./components/Input";
 
+
 const App = () => {
+
+
   const [text, setText] = useState("");
+
   const [result, setResult] = useState("");
 
-  const addToText = (val) => {
-    setText((text) => [...text, val + " "]);
-  };
 
-  const calculateResult = () => {
-    const input = text.join(""); // Remove commas
 
-    setResult(math.evaluate(input));
-  };
+  const addToText= (val) =>{
+    // setText((text) => [...text ,val+ " "]); // with this it only take single digit and also not take decimals also
+    setText((text) =>text+val ); 
+    
+  }
 
-  const resetInput = () => {
-    setText("");
-    setResult("");
-  };
 
-  const buttonColor = "#f2a33c";
+const resetInput = () => {
+  setText("");
+  setResult("");
+};
+
+
+
+
+const calculateResult =() =>{
+  // const input = text.join(''); // remove  commas
+
+    const input = text; // now use text as input insted of joining array
+  setResult(math.evaluate(input));
+}
+
+
+const buttonColor ="#f2a33c"
 
   return (
     <div className="App">
-      <div className="calc-wrapper">
-        <Input text={text} result={result} />
-        <div className="row">
-          <Button symbol="7" handleClick={addToText} />
-          <Button symbol="8" handleClick={addToText} />
-          <Button symbol="9" handleClick={addToText} />
-          <Button symbol="/" color={buttonColor} handleClick={addToText} />
+      <div className='calc-wrapper'>
+ 
+      <Input text={text} result={result}/>
+
+
+        <div className='row'>
+          <Button symbol="7" handelClick={addToText}/>
+          <Button symbol="8"handelClick={addToText}/>
+          <Button symbol="9"handelClick={addToText}/>
+          <Button symbol="/" color={buttonColor} handelClick={addToText}/>
         </div>
-        <div className="row">
-          <Button symbol="4" handleClick={addToText} />
-          <Button symbol="5" handleClick={addToText} />
-          <Button symbol="6" handleClick={addToText} />
-          <Button symbol="*" color={buttonColor} handleClick={addToText} />
+          
+
+        <div className='row'>
+          <Button symbol="4"handelClick={addToText}/>
+          <Button symbol="5"handelClick={addToText}/>
+          <Button symbol="6"handelClick={addToText}/>
+          <Button symbol="*" color={buttonColor} handelClick={addToText}/>
         </div>
-        <div className="row">
-          <Button symbol="1" handleClick={addToText} />
-          <Button symbol="2" handleClick={addToText} />
-          <Button symbol="3" handleClick={addToText} />
-          <Button symbol="+" color={buttonColor} handleClick={addToText} />
+
+
+        <div className='row'>
+          <Button symbol="1"handelClick={addToText}/>
+          <Button symbol="2"handelClick={addToText}/>
+          <Button symbol="3"handelClick={addToText}/>
+          <Button symbol="+" color={buttonColor} handelClick={addToText}/>
         </div>
-        <div className="row">
-          <Button symbol="0" handleClick={addToText} />
-          <Button symbol="." handleClick={addToText} />
-          <Button symbol="=" handleClick={calculateResult} />
-          <Button symbol="-" color={buttonColor} handleClick={addToText} />
+
+
+        <div className='row'>
+          <Button symbol="0"handelClick={addToText}/>
+          <Button symbol="."handelClick={addToText}/>
+          <Button symbol="=" handelClick={calculateResult}/>
+          <Button symbol="-" color={buttonColor} handelClick={addToText}/>
         </div>
-        <Button symbol="Clear" color="red" handleClick={resetInput} />
+
+
+        <Button symbol="Clear" color={"red"} handelClick={resetInput}/>
+
       </div>
     </div>
   );
 };
 
 export default App;
+
